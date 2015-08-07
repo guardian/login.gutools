e98804d303e14691559149456892058a29295982
Chris Finch <chris.finch@guardian.co.uk>
2015-08-07 14:47:08 +0100

    use a whiltelist

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,20 +1,26 @@
 package controllers
 
+import com.netaporter.uri.Uri
 import controllers.Login._
 import play.api._
 import play.api.mvc._
+import com.netaporter.uri.Uri.parse
+import com.netaporter.uri.dsl._
+import play.api.Play.current
 
 object Application extends Controller {
 
+  lazy val whitelist = play.api.Play.configuration.getStringList("redirect.whitelist")
+
   def isGuToolsDomain(returnUrl: String): Boolean = {
-    returnUrl.contains("gutools.co.uk") // Not a strict check at all..
+    whitelist.get.contains(returnUrl)
   }
 
   def login(returnUrl: String) = AuthAction { request =>
     if (isGuToolsDomain(returnUrl)) {
       Redirect(returnUrl)
     } else {
-      Ok("Not a gutools.co.uk domain")
+      Ok("Not a whitelisted gutools.co.uk domain")
     }
 
   }
