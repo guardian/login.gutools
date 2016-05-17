1b1dc07382e34fc42a1b6063cd7e5125c3e7c9a4
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2016-05-17 17:15:49 +0100

    Put s3 bucket into config

--- app/config/Switches.scala
+++ app/config/Switches.scala
@@ -8,13 +8,15 @@ import org.quartz.impl.StdSchedulerFactory
 import play.api.Logger
 import play.api.libs.json.{Format, Json}
 import play.json.extra.Variants
+import play.api.Play.current
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
 
 object Switches {
   def allSwitches: Map[String, SwitchState] = agent.get()
   private val agent = Agent[Map[String, SwitchState]](Map.empty)
-  private val bucket = "login-gutools-config"
+  private val bucketOpt = play.api.Play.configuration.getString("s3.login.config")
   private val scheduler = StdSchedulerFactory.getDefaultScheduler
   val loginConfig = LoginConfig.loginConfig(AWS.eC2Client)
   val fileName = s"${loginConfig.stage.toUpperCase}/switches.json"
@@ -29,22 +31,26 @@ object Switches {
   def setEmergencySwitch(state: SwitchState): Option[Unit] = {
     val name = "emergency"
     val newStates = allSwitches + (name -> state)
-
     val json = Json.toJson(newStates)
     val jsonString = Json.stringify(json)
     val metaData = new ObjectMetadata()
     metaData.setContentLength(jsonString.getBytes("UTF-8").length)
-    val request = new PutObjectRequest(bucket, fileName, new StringInputStream(jsonString), metaData)
-    try {
-      val result = AWS.s3Client.putObject(request)
-      Logger.info(s"$name has been updated to ${state.name}")
-      agent.send(newStates)
-      Some(Unit)
-    } catch {
-      case e: Exception => {
-        Logger.error(s"Unable to update switch $name ${state.name}", e)
-        None
+    bucketOpt.map { bucket =>
+      try {
+        val request = new PutObjectRequest(bucket, fileName, new StringInputStream(jsonString), metaData)
+        AWS.s3Client.putObject(request)
+        Logger.info(s"$name has been updated to ${state.name}")
+        agent.send(newStates)
+        Some(())
+      } catch {
+        case e: Exception => {
+          Logger.error(s"Unable to update switch $name ${state.name}", e)
+          None
+        }
       }
+    }.getOrElse {
+      Logger.error(s"S3 bucket login.gutools config not defined. Unable to update switch $name ${state.name}")
+      None
     }
   }
 
@@ -70,21 +76,23 @@ object Switches {
     scheduler.deleteJob(job.getKey)
   }
 
-  def refresh() = {
+  def refresh() {
     Logger.debug("Refreshing switches agent")
-    try {
-      val request = new GetObjectRequest(bucket, fileName)
-      val result = AWS.s3Client.getObject(request)
-      val source = Source.fromInputStream(result.getObjectContent).mkString
-      val statesInS3 = Json.parse(source).as[Map[String, SwitchState]]
+    bucketOpt.map { bucket =>
+      try {
+        val request = new GetObjectRequest(bucket, fileName)
+        val result = AWS.s3Client.getObject(request)
+        val source = Source.fromInputStream(result.getObjectContent).mkString
+        val statesInS3 = Json.parse(source).as[Map[String, SwitchState]]
 
-      agent.send(statesInS3)
-      result.close()
-    }
-    catch {
-      case e: Exception =>
-        Logger.error(s"Unable to get an updated version of switches.json from S3 $bucket $fileName. The switches map is likely to be stale. ", e)
-    }
+        agent.send(statesInS3)
+        result.close()
+      }
+      catch {
+        case e: Exception =>
+          Logger.error(s"Unable to get an updated version of switches.json from S3 $bucket $fileName. The switches map is likely to be stale. ", e)
+      }
+    }.getOrElse(Logger.error(s"Unable to get an updated version of switches.json, login.gutools config not defined."))
   }
 }
 
