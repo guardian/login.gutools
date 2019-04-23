590acb600df0361980623faef9d97b6d48293571
Akash Askoolum <akash1810@gmail.com>
2019-04-23 13:09:36 +0100

    update email message

--- app/utils/Mailer.scala
+++ app/utils/Mailer.scala
@@ -9,16 +9,30 @@ class SES(sesClient: AmazonSimpleEmailService, loginConfig: LoginConfig) {
 
     val uri = loginConfig.tokenReissueUri
 
-    val emailBody = s"<div>Click <a href=$uri$token>here</a> here to obtain a new cookie</div>"
+    val emailBody =
+      s"""
+         |<div>
+         |  Hi,
+         |  <p>
+         |    The Guardian's Editorial Tools are currently experiencing issues with Google Authentication and so we cannot authenticate your access to the Tools.
+         |  </p>
+         |  <p>
+         |    Click <a href=$uri$token>here</a> to obtain a new cookie to allow you to use them.
+         |  </p>
+         |  <p>
+         |    <strong>Do NOT share this email.</strong>
+         |  </p>
+         |</div>
+       """.stripMargin
 
     sesClient.sendEmail(new SendEmailRequest()
       .withDestination(new Destination().withToAddresses(sendTo))
       .withMessage(new Message()
-        .withSubject(new Content("Gutools new cookie link"))
+        .withSubject(new Content("[emergency login] Guardian Editorial Tools - new cookie link"))
         .withBody(new Body().withHtml(new Content(emailBody)))
       )
-      .withSource(loginConfig.emailSettings.get("from").get)
-      .withReplyToAddresses(loginConfig.emailSettings.get("replyTo").get)
+      .withSource(loginConfig.emailSettings("from"))
+      .withReplyToAddresses(loginConfig.emailSettings("replyTo"))
     )
 
   }
