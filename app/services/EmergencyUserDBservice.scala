9c165e3df6ff5d6c7e33491c856bc2c928d04421
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-01-04 07:54:17 +0000

    Add new services for DB actions

---
+++ app/services/EmergencyUserDBservice.scala
@@ -0,0 +1,25 @@
+package services
+
+import org.scanamo.{Scanamo, Table}
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import org.scanamo._
+import org.scanamo.syntax._
+import org.scanamo.generic.auto._
+import scala.concurrent.{ExecutionContext}
+import scala.util.Either
+
+case class EmergencyUser(userId: String, passwordHash: String)
+
+/**
+  * Service to manage persistence of emergency user records.
+  */
+class EmergencyUserDBService(
+  client: DynamoDbClient,
+  tableName: String
+)(implicit executionContext: ExecutionContext) {
+
+  private val table = Table[EmergencyUser](tableName)
+  private def run[T] = Scanamo(client).exec[T] _
+
+  def getUser(userId: String): Option[Either[DynamoReadError, EmergencyUser]] = run(table.get("userId" === userId))
+}
---
+++ app/services/TokenDBService.scala
@@ -0,0 +1,34 @@
+package services
+
+import org.scanamo.{Scanamo, Table}
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import org.scanamo._
+import org.scanamo.syntax._
+import org.scanamo.generic.auto._
+import scala.concurrent.{ExecutionContext}
+import scala.util.Either
+
+case class NewCookieIssue(id: String, email: String, requested: Long, used: Boolean)
+
+/**
+  * Service to manage persistence of user cookies.
+  */
+class TokenDBService(
+  client: DynamoDbClient,
+  tableName: String
+)(implicit executionContext: ExecutionContext) {
+
+  private val table = Table[NewCookieIssue](tableName)
+  private def run[T] = Scanamo(client).exec[T] _
+
+  def getCookieIssueForUserToken(userToken: String): Option[Either[DynamoReadError, NewCookieIssue]] =
+    run(table.get("id" === userToken))
+
+  def createCookieIssue(cookieIssue: NewCookieIssue): Unit = {
+    run(table.put(cookieIssue))
+  }
+
+  def expireCookieIssue(cookieIssue: NewCookieIssue): Unit = {
+    run(table.put(cookieIssue.copy(used = true)))
+  }
+}
