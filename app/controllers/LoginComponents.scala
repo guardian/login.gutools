b2459cf8d44369b0d6681bda67919dab7bb1c57c
Sam Hession <samhession@live.co.uk>
2025-04-21 12:38:04 +0100

    Removes setting the emergency switch via the backend

--- app/controllers/LoginComponents.scala
+++ app/controllers/LoginComponents.scala
@@ -15,7 +15,7 @@ import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.mvc._
 import play.filters.csrf.CSRFComponents
 import play.filters.headers.SecurityHeadersComponents
-import services.{EmergencyUser, EmergencyUserDBService, TokenDBService}
+import services.{TokenDBService}
 import utils.Loggable
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -32,7 +32,6 @@ abstract class LoginControllerComponents(
 
   def httpFilters: Seq[EssentialFilter] = Seq(csrfFilter, securityHeadersFilter)
 
-  lazy val emergencyUserDBService = new EmergencyUserDBService(aws.dynamoDbClient, config.emergencyAccessTableName)
   lazy val tokenDBService = new TokenDBService(aws.dynamoDbClient, config.tokensTableName)
 
   def config: LoginConfig
@@ -81,68 +80,4 @@ abstract class LoginController(
       }
     }
   }
-
-  object EmergencySwitchChangeAccess extends ActionBuilder[Request, AnyContent] {
-    final override def parser: BodyParser[AnyContent] = deps.controllerComponents.parsers.default
-    final override def executionContext: ExecutionContext = deps.executionContext
-
-    override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
-
-      def checkPassword(user: EmergencyUser, username: String, password: String): Future[Result] = {
-        if (password.isBcryptedBounded(user.passwordHash)) {
-          log.info(s"$username is authorised to change the Emergency switch.")
-          block(request)
-        } else {
-          refuseSwitchChange(s"The password provided by $username is incorrect. User will be refused access to change emergency switch.")
-        }
-      }
-
-      def refuseSwitchChange(logErrorMsg: String): Future[Result] = {
-        log.warn(logErrorMsg)
-        Future.successful {
-          Unauthorized(
-            views.html.switches.switchChange(
-              "Authorisation checks failed, the Emergency switch will not be changed. Contact digitalcms.dev@theguardian.com for more help."
-            ))
-        }
-      }
-
-      try {
-        val authHeaderUser = EmergencyActions.getBasicAuthDetails(request.headers)
-        val userId = authHeaderUser.id
-        val userOpt = deps.emergencyUserDBService.getUser(userId)
-        userOpt.map {
-          case Left(error) => refuseSwitchChange(s"Error with reading $userId from Dynamo: ${error.toString}. User will be refused access to change emergency switch.")
-          case Right(user) => checkPassword(user, userId, authHeaderUser.password)
-        }.getOrElse(refuseSwitchChange(s"User $userId not found. User will be refused access to change emergency switch."))
-      } catch {
-        case e: EmergencyActionsException =>
-          refuseSwitchChange(e.getMessage)
-      }
-    }
-  }
 }
-
-object EmergencyActions {
-  def getBasicAuthDetails(headers: Headers): AuthorizationHeaderUser = {
-    val authUserOpt = for {
-      authHeaders <- headers.toMap.get("Authorization")
-      basicAuthHead <- authHeaders.find(_.startsWith("Basic"))
-    } yield {
-      val basicAuthHeaderValue = basicAuthHead.split("Basic")(1).trim
-      if (!basicAuthHeaderValue.contains(":")) {
-        throw new EmergencyActionsException("Authorization header value is not the correct format.")
-      }
-      val usernameAndPassword = basicAuthHeaderValue.split(":")
-      if (usernameAndPassword.length != 2 || !usernameAndPassword(0).endsWith("@guardian.co.uk")) {
-        throw new EmergencyActionsException("Authorization header value is not the correct format.")
-      }
-      AuthorizationHeaderUser(usernameAndPassword(0).split("@guardian.co.uk")(0), usernameAndPassword(1))
-    }
-    authUserOpt.getOrElse(throw new EmergencyActionsException("Basic authorization header is missing"))
-  }
-}
-
-case class AuthorizationHeaderUser(id: String, password: String)
-
-class EmergencyActionsException(message: String) extends Exception(message: String)
